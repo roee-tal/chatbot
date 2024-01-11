4073201efcd707bc94c6173a2e545b3d12ff1222
roee-tal <roeetal979@gmail.com>
2024-01-11 13:50:02 +0200

    with swagger

--- src/main/java/com/handson/chatbot/controller/BotController.java
+++ src/main/java/com/handson/chatbot/controller/BotController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
+
 @Service
 @RestController
 @RequestMapping("/bot")
