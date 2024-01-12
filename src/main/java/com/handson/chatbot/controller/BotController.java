fb4cfe23112ca86cfabc60d1ede4e9082739c299
roee-tal <roeetal979@gmail.com>
2024-01-12 14:31:14 +0200

    final

--- src/main/java/com/handson/chatbot/controller/BotController.java
+++ src/main/java/com/handson/chatbot/controller/BotController.java
@@ -6,12 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.util.HashMap;
 
 
 @Service
@@ -32,5 +30,53 @@ public class BotController {
     @RequestMapping(value = "/norris", method = RequestMethod.GET)
     public ResponseEntity<?> getChuck() throws IOException {
         return new ResponseEntity<>(norrisService.getRes(), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "", method = { RequestMethod.POST})
+    public ResponseEntity<?> getBotResponse(@RequestBody BotQuery query) throws IOException {
+        HashMap<String, String> params = query.getQueryResult().getParameters();
+        String res = "Not found";
+        if (params.containsKey("city")) {
+            res = ""+norrisService.getRes();
+        } else if (params.containsKey("product")) {
+            res = amazonService.searchProducts(params.get("product"));
+        }
+        return new ResponseEntity<>(BotResponse.of(res), HttpStatus.OK);
+    }
+
+
+    static class BotQuery {
+        QueryResult queryResult;
+
+        public QueryResult getQueryResult() {
+            return queryResult;
+        }
+    }
+
+    static class QueryResult {
+        HashMap<String, String> parameters;
+
+        public HashMap<String, String> getParameters() {
+            return parameters;
+        }
+    }
+
+    static class BotResponse {
+        String fulfillmentText;
+        String source = "BOT";
+
+        public String getFulfillmentText() {
+            return fulfillmentText;
+        }
+
+        public String getSource() {
+            return source;
+        }
+
+        public static BotResponse of(String fulfillmentText) {
+            BotResponse res = new BotResponse();
+            res.fulfillmentText = fulfillmentText;
+            return res;
+        }
     }
 }
